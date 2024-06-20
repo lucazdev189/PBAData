1b3ccdddcad9cc86124e89cda09384eff3def427
Lucas E <erinlucaspogi@outlook.com>
2024-06-20 12:34:38 +0800

    fixed tests

--- tests/testthat/test-load_pba2023.R
+++ tests/testthat/test-load_pba2023.R
@@ -3,10 +3,10 @@ library(PBAData)
 library(tibble)
 
 test_that("shows correct dataset", {
-  PBA2024 <- load_pba2024()
+  PBA2023 <- load_pba2023()
   
-  expect_true(is_tibble(PBA2024))
+  expect_true(is_tibble(PBA2023))
   
   columns <- c("Name", "Joined", "Events", "Titles", "Average", "Earnings")
-  expect_true(all(columns %in% names(PBA2024)))
+  expect_true(all(columns %in% names(PBA2023)))
 })
