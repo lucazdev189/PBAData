d88cbb79ff37ae0ff815e8d5b2d1f4ddb68cf216
lucazdev189 <erinlucaspogi@outlook.com>
2024-06-22 10:05:28 +0800

    scatterplot_average

--- R/scatterplot_average.R
+++ R/scatterplot_average.R
@@ -4,8 +4,8 @@
 #' 
 #' @param dataset Dataset to be used to get info.
 #' @param y_axis The y axis values to be plotted.
-#' @param linear_regression Adds a linear regression line. Set to FALSE on default.
-#' @param lregression_method Method used to do the linear regression line. Set to lm on default
+#' @param linear_regression Adds a linear regression line. Set to FALSE by default.
+#' @param lregression_method Method used to do the linear regression line. Set to lm by default
 #' @importFrom ggplot2 ggplot geom_point geom_smooth
 #' @importFrom ggthemes theme_excel
 #' @export
