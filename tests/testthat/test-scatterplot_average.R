148bc156241056eabce061bbc7dc5d3843140661
lucazdev189 <erinlucaspogi@outlook.com>
2024-06-23 12:10:30 +0800

    edited the dependencies in some files

--- tests/testthat/test-scatterplot_average.R
+++ tests/testthat/test-scatterplot_average.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(ggthemes)
+library(PBAData)
+
 test_that("basic functionality", {
   sample_data <- data.frame(Average = c(1, 2, 3, 4, 5), Earnings = c(10, 15, 20, 25, 30))
   plot <- scatterplot_average(sample_data)
